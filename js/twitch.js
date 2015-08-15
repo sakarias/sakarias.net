a9c4d7c7601a601ac37c2d3ca6212bd714a1986e
John Johannessen <johnj@slaskdot.org>
2015-08-15 22:33:09 +0200

    Moved twitch player into javascript

--- js/twitch.js
+++ js/twitch.js
@@ -1,4 +1,4 @@
-$.getJSON("http://sakarias.net/cache/twitchStatus.json", function(obj) {    
+$.getJSON("http://sakarias.net/cache/twitchStatus.json", function(obj) {
   if ( obj.streamLive == 1) {
     var streamTitle     = obj.streamTitle;
     var streamGame      = obj.streamGame;
@@ -7,8 +7,11 @@ $.getJSON("http://sakarias.net/cache/twitchStatus.json", function(obj) {
 
     $('#twitchContainer').append('<span class="light">Live now : </span>' + streamGame + '<br />');
     $('#twitchContainer').append('<a href="http://twitch.tv/sakariaslp"><img src=' + streamPreview +' height="180" width="320"/></a>');
+    $('#twitchStream').append('<iframe width="720" height="405" src="http://twitch.tv/sakariaslp/embed" frameborder="0" allowfullscreen></iframe>');
   }
   else {
+    var streamPreview   = obj.streamPreview;
     $('#twitchContainer').append('<span class="light">Sakarias</span> Gaming');
+    $('#twitchStream').append('<img src="' + streamPreview +'" />');
   }
 });
