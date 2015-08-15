2a0fe2415a443cc041e887784cad0d7bdcdf0e4a
John Johannessen <johnj@slaskdot.org>
2015-08-16 01:10:36 +0200

    streamURL from json instead of hardcoding it

--- js/twitch.js
+++ js/twitch.js
@@ -3,11 +3,12 @@ $.getJSON("http://cache.sakarias.net/twitchStatus.json", function(obj) {
     var streamTitle     = obj.streamTitle;
     var streamGame      = obj.streamGame;
     var streamPreview   = obj.streamPreview;
+    var streamURL       = obj.streamURL;
     var streamStartTime = obj.streamStartTime;
 
     $('#twitchContainer').append('<span class="light">Live now : </span>' + streamGame + '<br />');
-    $('#twitchContainer').append('<a href="http://twitch.tv/sakariaslp"><img src=' + streamPreview +' height="180" width="320"/></a>');
-    $('#twitchStream').append('<iframe width="720" height="405" src="http://twitch.tv/sakariaslp/embed" frameborder="0" allowfullscreen></iframe>');
+    $('#twitchContainer').append('<a href="'+ streamURL +'"><img src=' + streamPreview +' height="180" width="320"/></a>');
+    $('#twitchStream').append('<iframe width="720" height="405" src="'+ streamURL +'/embed" frameborder="0" allowfullscreen></iframe>');
   }
   else {
     var streamPreview   = obj.streamPreview;
